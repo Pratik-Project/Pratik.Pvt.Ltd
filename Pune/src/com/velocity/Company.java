977c25970f092c67dba66c7a17f8aa8a453c2f0f
Pratik <Pratik@DESKTOP-GHAOC17.mshome.net>
2022-10-13 00:39:44 +0530

    first commit

---
+++ Pune/src/com/velocity/Company.java
@@ -0,0 +1,40 @@
+package com.velocity;
+
+public class Company {
+	
+	int a=25;
+	int b;
+	
+	static float e=40;
+	static float f;
+	char g='p';
+	
+	public static void main(String[] args) {
+		int c=30;
+		int d;
+		String h="pratik";
+		
+		Company company=new Company();
+		
+		System.out.println(company.a);
+		System.out.println(company.b);
+		
+		System.out.println(e);
+		System.out.println(f);
+		
+		System.out.println("The value of variable c is "+c);
+		System.out.println(company.g);
+		System.out.println(h);
+		System.out.println("The value of is a and b are"+"       " +company.a+"          "+company.b);
+		//Player player=new Player();
+		System.out.println(Player.z);
+		Player player=new Player();
+		System.out.println(player.x);
+		System.out.println(f);
+		
+		
+		
+		}
+	}
+		
+
